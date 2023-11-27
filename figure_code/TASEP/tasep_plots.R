fdc15fca3a663a2210eaacaec1e191883d4f01f8
Liana Lareau <lareau@berkeley.edu>
2023-11-26 19:06:33 -0800

    tasep colors

--- figure_code/TASEP/tasep_plots.R
+++ figure_code/TASEP/tasep_plots.R
@@ -1,7 +1,3 @@
-options(stringsAsFactors=FALSE)
-library("RColorBrewer")
-pal <- brewer.pal(11, "RdGy")
-
 datadir <- "data/TASEP/"
 figdir <- "figures/"
 
@@ -9,6 +5,9 @@ init <- read.delim( file.path( datadir, "scan_init_withlife.txt" ), header=TRUE)
 init$te_ratio <- with( init, (nprot_slow / mrna_slow) / (nprot_fast / mrna_fast) )
     
 cols <- c( citmin = 'magenta3', cit9 = 'darkorange2')
+
+cols2 <- c( citmin = rgb( t(col2rgb('magenta3')), alpha = 200, max = 255),
+            cit9 = rgb( t(col2rgb('darkorange2')), alpha = 200, max = 255) )
 
 pdf( file.path( figdir, "tasep_protein.pdf" ), width=1.75, height=1.3, pointsize = 6.5, useDingbats=FALSE, bg = "white")
 par( mex = 0.65 ) # sets margin stuff
@@ -20,7 +19,7 @@ plot( init$init_rate * 60, init$nprot_ratio,
       xlim = c(0, 6),
       ylim = c(0,1),
       pch = 20,
-      col = "darkgrey",
+      col = "black",
       xlab = "initiation [ribosomes / min]",
       ylab = "slow citrine as\nfraction of fast citrine",
       axes=F )
@@ -29,11 +28,11 @@ axis( 2, at = seq(0, 1.0, 0.25), labels = F, lwd = 0.75)
 axis( 1, at = seq(0, 6, 2), lwd = 0.75)
 axis( 1, at = seq(0, 6), labels = F, lwd = 0.75 )
 
-abline(h=0.65, lwd=0.8, col=pal[[8]], lty = 2)
-abline(h=0.32, lwd=0.8, col=pal[[10]], lty = 2)
+abline(h=0.65, lwd=0.8, col='#808285', lty = 2)
+abline(h=0.32, lwd=0.8, col='#4e4e4e', lty = 2)
 
-text(x = 6.25, y = 0.65, adj = c(1, -0.2), labels="ratio expected from\nmRNA abundance alone", col=pal[[8]], cex = 0.7)
-text(x = 6.25, y = 0.32, adj = c(1, 1.3), labels="observed ratio", col=pal[[10]], cex = 0.7)
+text(x = 6.25, y = 0.65, adj = c(1, -0.2), labels="ratio expected from\nmRNA abundance alone", col='#808285', cex = 0.7)
+text(x = 6.25, y = 0.32, adj = c(1, 1.3), labels="observed ratio", col='#4e4e4e', cex = 0.7)
 dev.off()
 
 
@@ -56,12 +55,6 @@ axis( 2, at = c(0, 0.5, 1.0), lwd = 0.75)
 axis( 2, at = seq(0, 1.0, 0.25), labels = F, lwd = 0.75)
 axis( 1, at = seq(0, 6, 2), lwd = 0.75)
 axis( 1, at = seq(0, 6), labels = F, lwd = 0.75 )
-
-# abline(h=0.65, lwd=0.8, col=pal[[8]], lty = 2)
-# abline(h=0.32, lwd=0.8, col=pal[[10]], lty = 2)
-# 
-# text(x = 6.25, y = 0.65, adj = c(1, -0.2), labels="ratio expected from\nmRNA abundance alone", col=pal[[8]], cex = 0.7)
-# text(x = 6.25, y = 0.32, adj = c(1, 1.3), labels="observed ratio", col=pal[[10]], cex = 0.7)
 dev.off()
 
 
@@ -76,18 +69,18 @@ plot( init$init_rate * 60,
       1 - init$collide_fast,
       xlim = c(0,6),
       ylim = c(0,1),
-      #pch=20, 
-      cex = 0.8,
-      col=cols[["citmin"]],
+      pch=16, 
+      cex = 0.6,
+      col=cols2[["citmin"]],
       xlab="initiation [ribosomes / min]", 
       ylab="fraction successful\ninitiation attempts",
       axes=FALSE )
 
 points(init$init_rate * 60, 
        1 - init$collide_slow,
-       #pch=20,
-       cex = 0.8,
-       col=cols[["cit9"]] )
+       pch=16,
+       cex = 0.6,
+       col=cols2[["cit9"]] )
 
 axis( 2, at=c(0, 0.5, 1.0), lwd = 0.75)
 axis( 2, at=seq(0,1.0,0.25), labels=FALSE, lwd = 0.75)
